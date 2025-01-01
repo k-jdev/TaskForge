7e67df42c531bfce4cfe2e937fd689605f867357
k-jdev <svv.fine@gmail.com>
2025-01-01 23:50:16 +0200

    Delete .history directory

--- .history/src/main_20250101133558.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
--- .history/tailwind.config_20250101135119.js
+++
@@ -1,9 +0,0 @@
-// filepath: /c:/Users/Vlad/Desktop/Vlad/Programming/2021-2023/React/TaskForge/tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
